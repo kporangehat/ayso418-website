282d606459b8e153dde289a5bcc302e1f7782661
KP <kporangehat@gmail.com>
2026-02-11 17:55:52 -0600

    update wagtail to 7.3 and django to 6.0 (and others)

--- website/settings/base.py
+++ website/settings/base.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'django.contrib.postgres',  # needed for wagtailsearch postgres search backend
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
     "debug_toolbar",
